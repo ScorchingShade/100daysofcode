2932f47d2e2d4643a388a54db82c43b454e86069
ScorchingShade <ankush.sharma.con@hotstar.com>
2023-03-17 03:38:15 +0530

    added frontend project first

---
+++ jsProjects/frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">E-Comm App</a>
+      </header>
+      <main>
+        List of Products
+      </main>
+    </div>
+  );
+}
+
+export default App;

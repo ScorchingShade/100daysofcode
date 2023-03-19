4350fb9d33f444a264f6aa51d48a65e5d111f3ce
ScorchingShade <ankush.sharma.con@hotstar.com>
2023-03-20 04:44:35 +0530

    fix: made routing modular in Backend

---
+++ jsProjects/backend/routers/itemRouter.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const expressAsyncHandler =require("express-async-handler");
+const {items} = require("../models/models.js");
+
+const itemRouter = express.Router()
+
+// List Items
+itemRouter.get("/list", expressAsyncHandler(async(req, res) => {
+    res.status(200).send(items);
+  }));
+
+
+
+  // Create a new item
+  itemRouter.post("/create", expressAsyncHandler(async(req, res) => {
+    const { name, url, price } = await req.body;
+    if (!name) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+  
+    // had to add url and price because otherwise the app would look too boring, and what ecomm item does not have price or image!!
+    const item = {
+      id: items.length + 1,
+      name,
+      url,
+      price,
+      created_at: new Date(),
+    };
+    items.push(item);
+    return res.status(201).json(item);
+  }));
+
+
+
+  module.exports=itemRouter

b70beeb73120f407c4acde0d8047275e49cee99b
ScorchingShade <ankush.sharma.con@hotstar.com>
2024-04-08 23:50:06 +0530

    second commit

---
+++ first_project/main.py
@@ -0,0 +1,7 @@
+def __brand_name_generator__():
+    print("Welcome to the brand name Generator.")
+    city = input("What is the name of the city you grew up in?\n")
+    pet = input("What's your pet's name\n")
+    print("Your brand name could be "+city+" "+pet)
+
+__brand_name_generator__()
